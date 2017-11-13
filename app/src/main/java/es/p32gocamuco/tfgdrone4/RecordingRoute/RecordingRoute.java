a88b5082d7937a549ef12642f171bb52af7790cc
metichi <metichi@gmail.com>
2017-11-13 10:40:17 +0100

    Continued implementation or RecordingRoute

--- app/src/main/java/es/p32gocamuco/tfgdrone4/RecordingRoute/RecordingRoute.java
+++ app/src/main/java/es/p32gocamuco/tfgdrone4/RecordingRoute/RecordingRoute.java
@@ -2,6 +2,7 @@ package es.p32gocamuco.tfgdrone4.RecordingRoute;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.ListIterator;
 
 /**
  * This class represents a complete recording route.
@@ -17,17 +18,69 @@ public class RecordingRoute implements Serializable, RecordingTechnique.OnTechni
     private RoutePoint home;
     private ArrayList<RecordingTechnique> recordingTechniques;
     private Constrains constrains;
+    private MapChangeListener listener;
 
+    public RecordingRoute(RoutePoint home, Constrains constrains){
+        this.home = home;
+        this.constrains = constrains;
+        this.recordingTechniques = new ArrayList<>();
+        this.listener = null;
+
+    }
+
+
+    //region Setters
+
+    public void setConstrains(Constrains constrains) {
+        this.constrains = constrains;
+    }
+
+    public void setHome(RoutePoint home) {
+        this.home = home;
+    }
+
+    public void setMapChangeListener(MapChangeListener listener) {
+        this.listener = listener;
+    }
+
+    //endregion
+
+    /**
+     * Adds a new technique to the end of the technique list.
+     * The RoutePoints in this technique will be placed in order at the end of the route.
+     * Should this technique have Targets or RoutePoints already, and if the MapChangeListener has
+     * been set, the methods {@link MapChangeListener#onNewRoutePoint(RoutePoint)} and {@link MapChangeListener#onNewTarget(Target)}
+     * will be called for each of them.
+     * @param technique Technique to add to the route.
+     */
+    public void addTechnique(RecordingTechnique technique){
+        recordingTechniques.add(technique);
+        if(listener != null) {
+            ArrayList<Target> targets = technique.getTargets();
+            if(!targets.isEmpty()){
+                for(Target target : targets){
+                    listener.onNewTarget(target);
+                }
+            }
+
+            ArrayList<RoutePoint> routePoints = technique.getRoutePoints();
+            if(!routePoints.isEmpty()){
+                for(RoutePoint routePoint : routePoints){
+                    listener.onNewRoutePoint(routePoint);
+                }
+            }
+        }
+        technique.setListener(this);
+    }
 
     /**
      * This method calculates the minimum time it would take an aircraft to go between two routepoints
      * if it adheres to the constrains.
      * @param origin Start point.
      * @param destination End point.
-     * @param constrains Constrains to follow.
      * @return Minimum travel time between the points in seconds.
      */
-    public double minTravelTimeBetween(RoutePoint origin, RoutePoint destination, Constrains constrains){
+    public double minTravelTimeBetween(RoutePoint origin, RoutePoint destination){
         double minTime = 0;
         //Min time because of travel distance
         double distance = origin.calculateFocalDistanceTo(destination);
@@ -47,6 +100,160 @@ public class RecordingRoute implements Serializable, RecordingTechnique.OnTechni
         return minTime;
     }
 
+    /**
+     * Calculates the travel speed between two routepoints.
+     * This method asumes the drone moves in a straight line between the two routepoints at a constant
+     * speed.
+     * The time of travel is considered as the traveltime of the destination.
+     * The distance is obtained by calling {@link RoutePoint#getFocalDistance()}.
+     * @param origin Routepoint of origin.
+     * @param destination Routepoint of destination.
+     * @return Speed of travel in meters per second.
+     *
+     */
+    public static double getTravelSpeed(RoutePoint origin, RoutePoint destination){
+        double distance = origin.calculateFocalDistanceTo(destination);
+        double speed = distance/destination.getTravelTime();
+        return speed;
+    }
+
+    /**
+     * Returns a complete list of all the RoutePoints in the Route.
+     * @return All RoutePoints contained in all techniques.
+     */
+    public ArrayList<RoutePoint> getAllRoutePoints(){
+        ArrayList<RoutePoint> routePoints = new ArrayList<>();
+        routePoints.add(this.home);
+        for (RecordingTechnique technique : this.recordingTechniques){
+            routePoints.addAll(technique.getRoutePoints());
+        }
+        return routePoints;
+    }
+
+    //region Action management
+
+    /**
+     * This method fixes the actions on the route.
+     * All the routepoints are checked in order, and determines wether that routepoint is recording or not.
+     * It is recording if a previoius routepoint has started a recording and no other Routepoint has stopped it,
+     * the only actions aviable are "stop recording" and "do nothing".
+     * If its not recording, then the aviable actions are "take a picture", "start recording" and "do nothing".
+     */
+    public void fixActions(){
+        ArrayList<RoutePoint> routePoints = this.getAllRoutePoints();
+        boolean isRecording = false;
+        for (RoutePoint routePoint : routePoints){
+            if (isRecording){
+                switch (routePoint.getAction()){
+                    case STOP_RECORDING:
+                        isRecording = false;
+                        break;
+                    default:
+                        routePoint.setAction(RoutePoint.Actions.NOTHING);
+                }
+            } else {
+                switch (routePoint.getAction()){
+                    case START_RECORDING:
+                        isRecording = true;
+                        break;
+                    case STOP_RECORDING:
+                        routePoint.setAction(RoutePoint.Actions.NOTHING);
+                        break;
+                }
+            }
+        }
+        if (isRecording){
+            RoutePoint lastRoutePoint = routePoints.get(routePoints.size()-1);
+            lastRoutePoint.setAction(RoutePoint.Actions.STOP_RECORDING);
+        }
+    }
+
+    /**
+     * This method checks wether the provided routepoint is recording.
+     * A point is recording if a previous routepoint started a recording and neither it or a routePoint
+     * in between stopped it.
+     * @param routePoint RoutePoint of Interest
+     * @return Wether it is recording.
+     */
+    public boolean isRecording(RoutePoint routePoint){
+        ArrayList<RoutePoint> routePoints = this.getAllRoutePoints();
+        boolean isRecording = false;
+        for (RoutePoint r : routePoints){
+            if (isRecording){
+                if (r.getAction() == RoutePoint.Actions.STOP_RECORDING){
+                    isRecording = false;
+                }
+            } else {
+                if(r.getAction() == RoutePoint.Actions.START_RECORDING){
+                    isRecording = true;
+                }
+            }
+
+            if(r == routePoint){
+                break;
+            }
+        }
+        return isRecording;
+    }
+    //endregion
+
+    //region Interface implementation
+
+    @Override
+    public void onNewTarget(Target target, RecordingTechnique technique) {
+        if (listener != null){
+            listener.onNewTarget(target);
+        }
+    }
+
+    @Override
+    public void onTargetChanged(Target target, RecordingTechnique technique) {
+        if (listener != null){
+            listener.onTargetChanged(target);
+        }
+    }
+
+    @Override
+    public void onTargetDeleted(Target target, RecordingTechnique technique) {
+        if (listener != null){
+            listener.onTargetRemoved(target);
+        }
+    }
+
+    @Override
+    public void onNewRoutePoint(RoutePoint routePoint, RecordingTechnique technique) {
+        RoutePoint previous = getAllRoutePoints().get(getAllRoutePoints().indexOf(routePoint)-1);
+        double minTravelTime = this.minTravelTimeBetween(previous,routePoint);
+        if (routePoint.getTravelTime()< minTravelTime){
+            routePoint.setTravelTime(minTravelTime);
+        }
+
+        if (listener != null){
+            listener.onNewRoutePoint(routePoint);
+        }
+    }
+
+    @Override
+    public void onRoutePointChanged(RoutePoint routePoint, RecordingTechnique technique) {
+
+
+        if (listener != null){
+            listener.onRoutePointChanged(routePoint);
+        }
+    }
+
+    @Override
+    public void onRoutePointDeleted(RoutePoint routePoint, RecordingTechnique technique) {
+        if (listener != null){
+            listener.onRoutePointRemoved(routePoint);
+        }
+    }
+
+    @Override
+    public void onTechniqueParametersChanged(RecordingTechnique recordingTechnique) {
+
+    }
+    //
 
     /**
      * This interface is built to manage the representation of the coordinated elements in a map.
@@ -64,7 +271,11 @@ public class RecordingRoute implements Serializable, RecordingTechnique.OnTechni
 
     /**
      * This class is used to hold the constrains on a Route.
-     * The constrains limit the maximum speed of the aircraft
+     * The constrains limit the maximum speed of the aircraft, as well as the rate of change for its angular
+     * parameters.
+     * It also defines an allowed flight volume, the volume will be a cilinder centered on the home
+     * position that spans from {@link Constrains#getMinHeight()} to {@link Constrains#getMaxHeight()}
+     * and has a radius of {@link Constrains#getMaxDistance()}.
      */
     public class Constrains{
         private double maxSpeed;
@@ -74,6 +285,15 @@ public class RecordingRoute implements Serializable, RecordingTechnique.OnTechni
         private double maxHeight;
         private double maxDistance;
 
+        /**
+         * Basic constructor for the Constrains group.
+         * @param maxSpeed Maximum speed allowed in a straight line in any direction in meters per second.
+         * @param maxPitchSpeed Maximum rate of change of the pitch angle, in degrees per second.
+         * @param maxBearingSpeed Maximum rate of change of the attitude, in degrees per second.
+         * @param minHeight Minimum height the drone is allowed to fly at, in meters, relative to takeoff point.
+         * @param maxHeight Maximum height the drone is allowed to fly at, in meters, relative to takeoff point.
+         * @param maxDistance Maximum distance the drone is allowed to fly away from Home. In meters.
+         */
         public Constrains(double maxSpeed, double maxPitchSpeed, double maxBearingSpeed, double minHeight, double maxHeight, double maxDistance){
             this.maxSpeed = maxSpeed;
             this.maxPitchSpeed = maxPitchSpeed;
